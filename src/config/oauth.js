142a57d499382f5b05f92de95f25e8dc81d25fcc
kangsungyong <smu201111192@gmail.com>
2019-08-11 23:17:56 +0900

    add oauth test code, some bug fix

--- src/config/oauth.js
+++ src/config/oauth.js
@@ -1,3 +1,8 @@
-export const googleClientId = process.env.GOOGLE_CLIENT_ID || '827093541876-dmlpnmhhd9vn54vjvfcarq119picf00a.apps.googleusercontent.com';
 export const facebookAppSecret = process.env.FACEBOOK_APP_SECRET || '1244e4ea33c459fd0df75549021f81d0';
 export const facebookAppId = process.env.FACEBOOK_APP_ID || '655653578274430';
+
+export const googleClientId = {
+  test: process.env.GOOGLE_CLIENT_TEST_ID || '827093541876-1nlrida7710p479c493v8oelvd3hl720.apps.googleusercontent.com',
+  ios: process.env.GOOGLE_CLIENT_IOS_ID || '827093541876-dmlpnmhhd9vn54vjvfcarq119picf00a.apps.googleusercontent.com',
+  android: process.env.GOOGLE_CLIENT_ANDROID_ID || '827093541876-0loli0g0pcgccovilkelvkc1m7jova08.apps.googleusercontent.com',
+};
